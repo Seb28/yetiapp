d7c5a06c76aa0cea666eed0fd319229145ef8d07
Sebastien <work.sebastien.david@gmail.com>
2020-08-04 23:20:55 +0200

    uuid

--- packages/printer-api/src/controllers/nodes.controller.ts
+++ packages/printer-api/src/controllers/nodes.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
+import { v4 as uuid } from 'uuid';
 
 export class NodesController {
   public index(req: Request, res: Response) {
+    const requestId = uuid();
     res.json({
-      message: "Hello boi"
+      identifier: requestId
     });
   }
 }
