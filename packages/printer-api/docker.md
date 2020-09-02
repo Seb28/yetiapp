c7ce464a548d82699b500f0219c4cca2cb6669f1
Sebastien <work.sebastien.david@gmail.com>
2020-09-02 17:47:30 +0200

    WIP

---
+++ packages/printer-api/docker.md
@@ -0,0 +1,6 @@
+Commande Docker a lancer pour créer l'image
+
+Après avoir construit le code : npm run build
+
+docker build --tag printerapi:1.4 .
+docker run --publish 8000:3000 --detach --name printer-api printerapi:1.4
---
+++ packages/printer-api/src/app.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var bodyParser = require("body-parser");
+var routes_1 = require("./config/routes");
+var App = /** @class */ (function () {
+    function App() {
+        this.routePrv = new routes_1.Routes();
+        this.app = express();
+        this.config();
+        this.routePrv.routes(this.app);
+    }
+    App.prototype.config = function () {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    };
+    return App;
+}());
+exports["default"] = new App().app;
